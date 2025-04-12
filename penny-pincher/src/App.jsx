38fd4d83621c68a1191a7f726b8d4797b470d8a0
Danie G. <158207372+danieog@users.noreply.github.com>
2025-04-12 00:50:00 -0400

    i make penny pincher mwahaha

--- penny-pincher/src/App.jsx
+++ penny-pincher/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import ChatComponent from "./components/chatcomponents";
 
 const App = () => {
   return (
     <div className="max-w-screen-lg mx-auto bg-white shadow-md rounded-lg overflow-hidden">
       <div className="w-full max-w-lg bg-white shadow-lg rounded-lg overflow-hidden">
-        
+        <ChatComponent />
       </div>
+    </div>
   );
 };
 
---
+++ penny-pincher/src/components/chatcomponents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChatComponent = () => {
+  return (
+    <div className="flex flex-col h-[80vh] bg-white">
+      <h2 className="p-4 font-semibold text-lg text-center bg-blue-100 flex text-blue-800 justify-center items-center gap-2"
+        Penny Pincher <LuBot size {25} /> 
+        </h2>
+    </div>
+  );
+};
+
+export default ChatComponent;
